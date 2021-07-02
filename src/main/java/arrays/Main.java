21c7d63fe38c7aee1268fa0d15ddd82f231b8f6d
Mohneesh Damade <mohneesh.damade@gmail.com>
2021-07-02 15:42:28 +0530

    Hacker rank solutions arrays

--- src/main/java/arrays/Main.java
+++ src/main/java/arrays/Main.java
@@ -1,5 +1,7 @@
 package arrays;
 
+import arrays.primenumber.SieveOfEratosthenes;
+
 public class Main {
 
 	public static void main(String[] args) {
--- src/main/java/arrays/primenumber/PrimeNumber.java
+++ src/main/java/arrays/primenumber/PrimeNumber.java
@@ -1,4 +1,4 @@
-package arrays;
+package arrays.primenumber;
 
 import java.util.Scanner;
 
